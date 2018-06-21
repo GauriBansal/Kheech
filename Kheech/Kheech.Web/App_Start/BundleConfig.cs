b1161f25e1c710f87b38fada0884e8abe94dc434
Gauri <35777100+reach2gauri@users.noreply.github.com>
2018-06-20 19:32:28 -0500

    moment controller channges and some issue fixes

--- Kheech/Kheech.Web/App_Start/BundleConfig.cs
+++ Kheech/Kheech.Web/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace Kheech.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/moment-with-locales.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
--- Kheech/Kheech.Web/Controllers/HomeController.cs
+++ Kheech/Kheech.Web/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace Kheech.Web.Controllers
 {
-    [RoutePrefix("Home")]
+    [RoutePrefix("")]
     [AllowAnonymous]
     public class HomeController : Controller
     {
