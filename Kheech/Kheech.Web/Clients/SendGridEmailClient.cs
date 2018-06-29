2724e0b75ef36d8588a1529d46e176887fc7a1b3
Gauri Bansal <reach2gauri@gmail.com>
2018-06-29 15:41:27 -0500

    Adding support for sending Email with SendGrid API.

--- Kheech/Kheech.Web/Clients/SendGridEmailClient.cs
+++ Kheech/Kheech.Web/Clients/SendGridEmailClient.cs
@@ -8,29 +8,45 @@ using System.Threading.Tasks;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 using System.IO;
+using System.Web.Hosting;
 
 namespace Kheech.Web.Clients
 {
     public class SendGridEmailClient : IEmailClient
     {
-        private readonly string _apiKey;
+        private readonly SendGridClient _sendGridApi;
 
-        private readonly string _genericEmail = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Content/Emails/GenericEmail.html"));
+        private readonly string _genericEmail = File.ReadAllText(HostingEnvironment.MapPath("~/Content/Emails/GenericEmail.html"));
+        private readonly string _actionEmail = File.ReadAllText(HostingEnvironment.MapPath("~/Content/Emails/ActionEmail.html"));
 
-        public SendGridEmailClient()
+        public SendGridEmailClient(string apiKey)
         {
-            _apiKey = ConfigurationManager.AppSettings["SendGridApiKey"];
+            _sendGridApi = new SendGridClient(apiKey);
         }
 
-        public async Task SendEmailAsync(string toEmailAddress, string subject, string htmlBody)
+        public async Task SendEmailAsync(string toEmailAddress, string subject, string messageText)
         {
-            var message = _genericEmail.Replace("{{EmailBody}}", htmlBody);
-            var client = new SendGridClient(_apiKey);
-            var from = new EmailAddress("noreply@kheech.com", "Kheech");
+            var from = new EmailAddress("kheech@arkansascodingacademy.com", "Kheech");
+            var htmlBody = _genericEmail.Replace("{{EmailBody}}", messageText);
             var to = new EmailAddress(toEmailAddress);
-            var email = MailHelper.CreateSingleEmail(from, to, subject, "", htmlBody);
+            var email = MailHelper.CreateSingleEmail(from, to, subject, messageText, htmlBody);
 
-            await client.SendEmailAsync(email);
+            await _sendGridApi.SendEmailAsync(email);
+        }
+        
+        public async Task SendActionEmailAsync(string toEmailAddress, string toName, string subject, string messageText, string actionUrl, string actionText)
+        {
+            var from = new EmailAddress("kheech@arkansascodingacademy.com", "Kheech");
+            var to = new EmailAddress(toEmailAddress);
+            var htmlBody = _actionEmail
+                .Replace("{{Name}}", toName ?? "Kheech user")
+                .Replace("{{EmailBody}}", messageText)
+                .Replace("{{ActionLinkUrl}}", actionUrl)
+                .Replace("{{ActionLinkText}}", actionText);
+
+            var email = MailHelper.CreateSingleEmail(from, to, subject, messageText, htmlBody);
+
+            await _sendGridApi.SendEmailAsync(email);
         }
     }
 }
