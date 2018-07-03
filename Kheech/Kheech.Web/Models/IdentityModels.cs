d1f9a5f8b4dedca3004674d8eedd511d066dd18d
Gauri <35777100+reach2gauri@users.noreply.github.com>
2018-07-02 21:58:23 -0500

    Added the Name in Hello Message.

--- Kheech/Kheech.Web/Models/IdentityModels.cs
+++ Kheech/Kheech.Web/Models/IdentityModels.cs
@@ -10,11 +10,14 @@ namespace Kheech.Web.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public const string DisplayNameClaimType = "DisplayName";
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
+            userIdentity.AddClaim(new Claim(ClaimTypes.GivenName, FirstName));
             return userIdentity;
         }
 
